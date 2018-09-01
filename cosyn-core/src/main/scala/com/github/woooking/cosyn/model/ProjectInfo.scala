bc988c2655969ed0edf653af09267c1ce20cf5a7
woooking <woooking@qq.com>
2018-09-01 14:15:18 +0800

    corpus-path

--- cosyn-core/src/main/scala/com/github/woooking/cosyn/model/ProjectInfo.scala
+++ cosyn-core/src/main/scala/com/github/woooking/cosyn/model/ProjectInfo.scala
@@ -3,13 +3,56 @@ package com.github.woooking.cosyn.model
 import better.files.File
 import better.files._
 
-case class ProjectInfo(name: String, roots: List[File])
+case class ProjectInfo(name: String, root: File, specialRoots: List[File]) {
+    def roots: List[File] = {
+        root.listRecursively
+            .filter(_.isDirectory)
+            .filter(_.toString().endsWith("/src/main/java"))
+            .toList ++ specialRoots
+    }
+}
 
 object ProjectInfo {
     val projects = List(
-        ProjectInfo("Android-Universal-Image-Loader", List(
-            file"/home/woooking/lab/java-codes/Android-Universal-Image-Loader-master/library/src/main/java",
-            file"/home/woooking/lab/java-codes/Android-Universal-Image-Loader-master/sample/src/main/java"
-        ))
+        ProjectInfo(
+            "Android-Universal-Image-Loader",
+            file"/home/woooking/lab/java-codes/Android-Universal-Image-Loader-master",
+            List()
+        ),
+        ProjectInfo(
+            "AndroidUtilCode",
+            file"/home/woooking/lab/java-codes/AndroidUtilCode-master",
+            List()
+        ),
+        ProjectInfo(
+            "bamboo",
+            file"/home/woooking/lab/java-codes/bamboo-master",
+            List()
+        ),
+        ProjectInfo(
+            "butterknife",
+            file"/home/woooking/lab/java-codes/butterknife-master",
+            List()
+        ),
+        ProjectInfo(
+            "elasticsearch",
+            file"/home/woooking/lab/java-codes/elasticsearch-master",
+            List()
+        ),
+        ProjectInfo(
+            "glide",
+            file"/home/woooking/lab/java-codes/glide-master",
+            List()
+        ),
+        ProjectInfo(
+            "butterknife",
+            file"/home/woooking/lab/java-codes/butterknife-master",
+            List()
+        ),
+        ProjectInfo(
+            "butterknife",
+            file"/home/woooking/lab/java-codes/butterknife-master",
+            List()
+        ),
     )
 }
