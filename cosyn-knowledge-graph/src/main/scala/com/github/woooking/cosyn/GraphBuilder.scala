cccfe45bb3b4950db230097ab55674ce1093e61c
woooking <woooking@qq.com>
2018-11-20 10:20:47 +0800

    代码同步

--- cosyn-knowledge-graph/src/main/scala/com/github/woooking/cosyn/GraphBuilder.scala
+++ cosyn-knowledge-graph/src/main/scala/com/github/woooking/cosyn/GraphBuilder.scala
@@ -1,8 +1,12 @@
 package com.github.woooking.cosyn
 
 import better.files.File.home
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
+import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.`type`.ClassOrInterfaceType
+import com.github.javaparser.ast.body.{ClassOrInterfaceDeclaration, MethodDeclaration}
 import com.github.javaparser.resolution.UnsolvedSymbolException
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration
+import com.github.javaparser.resolution.types.ResolvedReferenceType
 import com.github.javaparser.symbolsolver.utils.SymbolSolverCollectionStrategy
 import com.github.woooking.cosyn.entity.{MethodEntity, TypeEntity}
 import org.neo4j.ogm.config.Configuration
@@ -15,13 +19,17 @@ object GraphBuilder {
     private val methodMapping = mutable.Map[String, MethodEntity]()
     private val typeMapping = mutable.Map[String, TypeEntity]()
 
-    def main(args: Array[String]): Unit = {
-        val projectRoot = new SymbolSolverCollectionStrategy().collect(home / "lab" / "poi-3.14" / "src" / "java" path)
-        val cus = projectRoot.getSourceRoots.asScala
-            .flatMap(_.tryToParseParallelized().asScala)
-            .filter(_.isSuccessful)
-            .map(_.getResult.get())
+    private def type2entity(decl: ClassOrInterfaceType) = {
+        val qualifiedName = decl.resolve().getQualifiedName
+        typeMapping(qualifiedName)
+    }
 
+    private def type2entity(decl: ResolvedReferenceTypeDeclaration) = {
+        val qualifiedName = decl.getQualifiedName
+        typeMapping(qualifiedName)
+    }
+
+    def buildTypeMapping(cus: Seq[CompilationUnit]) = {
         cus.flatMap(_.findAll(classOf[ClassOrInterfaceDeclaration]).asScala)
             .foreach(decl => {
                 val qualifiedName = decl.resolve().getQualifiedName
@@ -36,6 +44,36 @@ object GraphBuilder {
                     }
                 })
             })
+    }
+
+    def buildExtendRelation(cus: Seq[CompilationUnit]) = {
+        cus.flatMap(_.findAll(classOf[ClassOrInterfaceDeclaration]).asScala)
+            .foreach(decl => {
+                val qualifiedName = decl.resolve().getQualifiedName
+                val typeEntity = typeMapping(qualifiedName)
+                typeEntity.addExtendedTypes(decl.getExtendedTypes.asScala.map(type2entity).toSet.asJava)
+                typeEntity.addImplementedTypes(decl.getImplementedTypes.asScala.map(type2entity).toSet.asJava)
+            })
+    }
+
+    def buildMethodImplementRelation(cus: Seq[CompilationUnit]) = {
+        cus.flatMap(_.findAll(classOf[MethodDeclaration]).asScala)
+            .foreach(decl => {
+                val resolvedMethod = decl.resolve()
+                val qualifiedSignature = resolvedMethod.getQualifiedSignature
+                val methodEntity = methodMapping(qualifiedSignature)
+                val typeEntity = type2entity(resolvedMethod.declaringType())
+            })
+    }
+
+    def main(args: Array[String]): Unit = {
+        val projectRoot = new SymbolSolverCollectionStrategy().collect(home / "lab" / "poi-3.14" / "src" / "java" path)
+        val cus = projectRoot.getSourceRoots.asScala
+            .flatMap(_.tryToParseParallelized().asScala)
+            .filter(_.isSuccessful)
+            .map(_.getResult.get())
+
+        buildTypeMapping(cus)
 
         val configuration = new Configuration.Builder()
             .uri("bolt://localhost")
--- cosyn-knowledge-graph/src/main/scala/com/github/woooking/cosyn/entity/TypeEntity.java
+++ cosyn-knowledge-graph/src/main/scala/com/github/woooking/cosyn/entity/TypeEntity.java
@@ -1,14 +1,31 @@
 package com.github.woooking.cosyn.entity;
 
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.Set;
 
 @NodeEntity
 public class TypeEntity extends Entity {
     private String qualifiedName;
     private boolean isInterface;
 
+    @Relationship(type = "EXTENDS")
+    private Set<TypeEntity> extendedTypes;
+
+    @Relationship(type = "IMPLEMENTS")
+    private Set<TypeEntity> implementedTypes;
+
     public TypeEntity(String qualifiedName, boolean isInterface) {
         this.qualifiedName = qualifiedName;
         this.isInterface = isInterface;
+    }
+
+    public void addExtendedTypes(Set<TypeEntity> extendedTypes) {
+        this.extendedTypes.addAll(extendedTypes);
+    }
+
+    public void addImplementedTypes(Set<TypeEntity> implementedTypes) {
+        this.implementedTypes.addAll(implementedTypes);
     }
 }
